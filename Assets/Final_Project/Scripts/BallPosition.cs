862058027fb49d008fbb1855b082550b3201ebc9
david krauskopf-greene <davidkrauskopf-greene@10-19-82-235.dynapool.wireless.nyu.edu>
2018-07-26 17:38:32 -0400

    instantiated hoops

---
+++ Assets/Final_Project/Scripts/BallPosition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallPosition : MonoBehaviour
+{
+    // this script is attached to the healthbar canvas so it faces the main camera
+
+    void Update()
+    {
+        // keep the healthbar canvas looking at the camera at all times
+        transform.LookAt(Camera.main.transform);
+    }
+}
