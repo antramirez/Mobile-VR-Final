eb424776b08a1e9cfc83386d185cf0e3d822bf87
david krauskopf-greene <davidkrauskopf-greene@10-19-82-235.dynapool.wireless.nyu.edu>
2018-07-26 15:48:32 -0400

    initial files

---
+++ Assets/Final_Project/Scripts/Billboard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace A08dkg3
+{
+    public class Billboard : MonoBehaviour
+    {
+        // this script is attached to the healthbar canvas so it faces the main camera
+
+        void Update()
+        {
+            // keep the healthbar canvas looking at the camera at all times
+            transform.LookAt(Camera.main.transform);
+        }
+    }
+}
---
+++ Assets/Final_Project/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace A08dkg3
+{
+    public class Bullet : MonoBehaviour
+    {
+        // this script detects bullet collisions with the enemies or players and
+        // removes 10 points of health if an enemy or player gets hit
+
+        void OnCollisionEnter(Collision collision)
+        {
+            var hit = collision.gameObject;
+            var health = hit.GetComponent<Health>();
+            if (health != null)
+            {
+                // remove 10 points of health
+                health.TakeDamage(10);
+            }
+            // destroy the bullet after a collision
+            Destroy(gameObject);
+        }
+    }
+}
