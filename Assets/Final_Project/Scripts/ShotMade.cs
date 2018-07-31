a52c566b2b3bc8205b85599f405d0413c8dac576
Anthony Ramirez <ar4477@nyu.edu>
2018-07-31 18:12:43 -0400

    detects ball made

---
+++ Assets/Final_Project/Scripts/ShotMade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotMade : MonoBehaviour
+{
+
+    public GameObject ball;
+    public GameObject ballCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "BallCollider")
+        {
+            Destroy(ball);
+        }
+    }
+}
