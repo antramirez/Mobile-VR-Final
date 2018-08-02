e69dc8aadfb2c2411c6a25f982a283c4a50e06ae
david krauskopf-greene <davidkrauskopf-greene@davids-MacBook-Pro-24.local>
2018-08-01 22:19:04 -0400

    timer

---
+++ Assets/Final_Project/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    private float timeLeft;
+
+	// Use this for initialization
+	void Start () {
+        timeLeft = Time.time + 90f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float time = timeLeft - Time.time;
+        string sec = (time % 91).ToString("f1");
+        timerText.text = sec;
+	}
+}
