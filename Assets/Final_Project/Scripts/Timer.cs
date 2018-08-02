107392bf7284abb1657f97772a637a4c94b05c67
david krauskopf-greene <davidkrauskopf-greene@davids-MacBook-Pro-24.local>
2018-08-01 22:41:21 -0400

    game over message

--- Assets/Final_Project/Scripts/Timer.cs
+++ Assets/Final_Project/Scripts/Timer.cs
@@ -6,16 +6,35 @@ public class Timer : MonoBehaviour {
 
     public Text timerText;
     private float timeLeft;
+    private bool gameOver = false;
+    float time;
 
 	// Use this for initialization
 	void Start () {
-        timeLeft = Time.time + 90f;
+        time = 10f;
+        timeLeft = Time.time + 10f;
+        timerText.color = Color.white;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float time = timeLeft - Time.time;
-        string sec = (time % 91).ToString("f1");
+        if (time <= 0f)
+        {
+            Finish();
+        }
+        if (gameOver)
+        {
+            return;
+        }
+        time = timeLeft - Time.time;
+        string sec = (time % 11).ToString("f1");
         timerText.text = sec;
 	}
+
+    public void Finish()
+    {
+        gameOver = true;
+        timerText.color = Color.red;
+        timerText.text = "Game Over";
+    }
 }
