4aba3ddbd22f5f94253d5f17b5af9fe2abd8aba5
david krauskopf-greene <davidkrauskopf-greene@davids-MacBook-Pro-24.local>
2018-08-04 18:28:37 -0400

    revert back one commit

--- Assets/Final_Project/Scripts/CustomNetworkDiscovery.cs
+++ Assets/Final_Project/Scripts/CustomNetworkDiscovery.cs
@@ -1,167 +1,9 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.Networking;
-
-//public class CustomNetworkDiscovery : NetworkDiscovery {
-//    private bool _receivedBradcast = false;
-
-//    float playernum;
-//    bool playNumSet;
-
-
-//    public float playerNumber;
-
-//    public bool player1set;
-//    public bool player2set;
-
-//	private void Start()
-//	{
-//       // gsScript = GameObject.Find("GameState").GetComponent<GameState>();
-//        Initialize();
-//        //TODO
-//        // access Game State Script
-//	}
-
-//    public override void OnReceivedBroadcast(string fromAddress, string data)
-//    {
-//        if (!_receivedBradcast)
-//        {
-//            _receivedBradcast = true;
-//            base.OnReceivedBroadcast(fromAddress, data);
-//            Debug.Log("FromAddress: " + fromAddress + "  Data: " + data);
-//            string[] addressSplit  = fromAddress.Split(':');
-//            string[] dataSplit = data.Split(':');
-//            NetworkManager.singleton.networkAddress = addressSplit[addressSplit.Length - 1];
-//            NetworkManager.singleton.networkPort = int.Parse(dataSplit[dataSplit.Length - 1]);
-//            NetworkManager.singleton.StartClient();
-//        }
-//    }
-
-//    public void StartListeningBroadcast() {
-//        StartAsClient();
-//        //playernum = (int)Random.Range(1f,3.9f);
-//        //  SetPlayerNumber(1);
-//        // playNumSet = true;
-//        player2set = true;
-//    }
-
-//    public void StartAsHost () {
-//        NetworkManager.singleton.StartHost();
-//        StartAsServer();
-//      //  SetPlayerNumber(0);
-//        player1set = true;
-//       // playNumSet = true;
-//    }
-
-//    //public bool PlayerNumIsSet()
-//    //{
-//    //    return playNumSet;
-//    //}
-
-
-
-//    //public void SetPlayerNumber(float num){
-//    //    playernum = num;
-//    //}
-//    //public float GetPLayerNumber(){
-//    //    return playernum;
-//    //}
-
-//    public bool Player1Set()
-//    {
-//        return player1set;
-//    }
-//    public bool Player2Set()
-//    {
-//        return player2set;
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-////using System.Collections;
-////using System.Collections.Generic;
-////using UnityEngine;
-////using UnityEngine.Networking;
-
-////public class CustomNetworkDiscovery : NetworkDiscovery
-////{
-////    private bool _receivedBradcast = false;
-
-////    float playernum;
-////    bool playNumSet;
-
-////    private void Start()
-////    {
-////        Initialize();
-////        //TODO
-////        // access Game State Script
-////    }
-
-////    public override void OnReceivedBroadcast(string fromAddress, string data)
-////    {
-////        if (!_receivedBradcast)
-////        {
-////            _receivedBradcast = true;
-////            base.OnReceivedBroadcast(fromAddress, data);
-////            Debug.Log("FromAddress: " + fromAddress + "  Data: " + data);
-////            string[] addressSplit = fromAddress.Split(':');
-////            string[] dataSplit = data.Split(':');
-////            NetworkManager.singleton.networkAddress = addressSplit[addressSplit.Length - 1];
-////            NetworkManager.singleton.networkPort = int.Parse(dataSplit[dataSplit.Length - 1]);
-////            NetworkManager.singleton.StartClient();
-////        }
-////    }
-
-////    public void StartListeningBroadcast()
-////    {
-////        StartAsClient();
-////        playernum = (int)Random.Range(1f, 3.9f);
-////        playNumSet = true;
-////        // accessing the game state script, check if rand num exists
-
-////    }
-
-////    public void StartAsHost()
-////    {
-////        NetworkManager.singleton.StartHost();
-////        StartAsServer();
-////        SetPlayerNumber(0);
-////        playNumSet = true;
-////    }
-
-////    public bool PlayerNumIsSet()
-////    {
-////        return playNumSet;
-////    }
-////    public void SetPlayerNumber(float num)
-////    {
-////        playernum = num;
-////    }
-////    public float GetPLayerNumber()
-////    {
-////        return playernum;
-////    }
-////}
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-public class CustomNetworkDiscovery : NetworkDiscovery
-{
+
+public class CustomNetworkDiscovery : NetworkDiscovery {
     private bool _receivedBradcast = false;
 
     float playernum;
@@ -173,16 +15,13 @@ public class CustomNetworkDiscovery : NetworkDiscovery
     public bool player1set;
     public bool player2set;
 
-    public bool playerHost;
-    public bool playerClient;
-
-    private void Start()
-    {
-        // gsScript = GameObject.Find("GameState").GetComponent<GameState>();
+	private void Start()
+	{
+       // gsScript = GameObject.Find("GameState").GetComponent<GameState>();
         Initialize();
         //TODO
         // access Game State Script
-    }
+	}
 
     public override void OnReceivedBroadcast(string fromAddress, string data)
     {
@@ -191,7 +30,7 @@ public class CustomNetworkDiscovery : NetworkDiscovery
             _receivedBradcast = true;
             base.OnReceivedBroadcast(fromAddress, data);
             Debug.Log("FromAddress: " + fromAddress + "  Data: " + data);
-            string[] addressSplit = fromAddress.Split(':');
+            string[] addressSplit  = fromAddress.Split(':');
             string[] dataSplit = data.Split(':');
             NetworkManager.singleton.networkAddress = addressSplit[addressSplit.Length - 1];
             NetworkManager.singleton.networkPort = int.Parse(dataSplit[dataSplit.Length - 1]);
@@ -199,25 +38,20 @@ public class CustomNetworkDiscovery : NetworkDiscovery
         }
     }
 
-    public void StartListeningBroadcast()
-    {
+    public void StartListeningBroadcast() {
         StartAsClient();
         //playernum = (int)Random.Range(1f,3.9f);
         //  SetPlayerNumber(1);
         // playNumSet = true;
         player2set = true;
-        playerClient = true;
     }
 
-    public void StartAsHost()
-    {
+    public void StartAsHost () {
         NetworkManager.singleton.StartHost();
         StartAsServer();
-        //  SetPlayerNumber(0);
+      //  SetPlayerNumber(0);
         player1set = true;
-        playerHost = true;
-        print("playerHost set to " + playerHost + " after being updated in StartAsHost()");
-        // playNumSet = true;
+       // playNumSet = true;
     }
 
     //public bool PlayerNumIsSet()
@@ -236,27 +70,89 @@ public class CustomNetworkDiscovery : NetworkDiscovery
 
     public bool Player1Set()
     {
-        playerHost = true;
-        print("player1set in custom set to " + player1set);
         return player1set;
-
     }
     public bool Player2Set()
     {
-        playerClient = true;
-        print("player2set in custom set to " + player2set);
         return player2set;
     }
-
-    public bool PlayerIsHost()
-    {
-        print("playerHost in custom, when PlayerIsHost() is called is " + playerHost);
-        return playerHost;
-    }
-    public bool PlayerIsClient()
-    {
-        playerHost = false;
-        return playerClient;
-    }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Networking;
+
+//public class CustomNetworkDiscovery : NetworkDiscovery
+//{
+//    private bool _receivedBradcast = false;
+
+//    float playernum;
+//    bool playNumSet;
+
+//    private void Start()
+//    {
+//        Initialize();
+//        //TODO
+//        // access Game State Script
+//    }
+
+//    public override void OnReceivedBroadcast(string fromAddress, string data)
+//    {
+//        if (!_receivedBradcast)
+//        {
+//            _receivedBradcast = true;
+//            base.OnReceivedBroadcast(fromAddress, data);
+//            Debug.Log("FromAddress: " + fromAddress + "  Data: " + data);
+//            string[] addressSplit = fromAddress.Split(':');
+//            string[] dataSplit = data.Split(':');
+//            NetworkManager.singleton.networkAddress = addressSplit[addressSplit.Length - 1];
+//            NetworkManager.singleton.networkPort = int.Parse(dataSplit[dataSplit.Length - 1]);
+//            NetworkManager.singleton.StartClient();
+//        }
+//    }
+
+//    public void StartListeningBroadcast()
+//    {
+//        StartAsClient();
+//        playernum = (int)Random.Range(1f, 3.9f);
+//        playNumSet = true;
+//        // accessing the game state script, check if rand num exists
+
+//    }
+
+//    public void StartAsHost()
+//    {
+//        NetworkManager.singleton.StartHost();
+//        StartAsServer();
+//        SetPlayerNumber(0);
+//        playNumSet = true;
+//    }
+
+//    public bool PlayerNumIsSet()
+//    {
+//        return playNumSet;
+//    }
+//    public void SetPlayerNumber(float num)
+//    {
+//        playernum = num;
+//    }
+//    public float GetPLayerNumber()
+//    {
+//        return playernum;
+//    }
+//}
