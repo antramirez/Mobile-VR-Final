d0946e100e82ed83650cc88e15d432c0bb8fc5f5
david krauskopf-greene <davidkrauskopf-greene@davids-MacBook-Pro-24.local>
2018-08-04 14:45:17 -0400

    set player ids

--- Assets/Final_Project/Scripts/CustomNetworkDiscovery.cs
+++ Assets/Final_Project/Scripts/CustomNetworkDiscovery.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class CustomNetworkDiscovery : NetworkDiscovery {
+public class CustomNetworkDiscovery : NetworkDiscovery
+{
     private bool _receivedBradcast = false;
 
     float playernum;
     bool playNumSet;
 
-	private void Start()
-	{
+    bool player1set;
+    bool player2set;
+    bool player3set;
+    bool player4set;
+
+    private void Start()
+    {
         Initialize();
         //TODO
         // access Game State Script
-	}
+    }
 
     public override void OnReceivedBroadcast(string fromAddress, string data)
     {
@@ -23,7 +29,7 @@ public class CustomNetworkDiscovery : NetworkDiscovery {
             _receivedBradcast = true;
             base.OnReceivedBroadcast(fromAddress, data);
             Debug.Log("FromAddress: " + fromAddress + "  Data: " + data);
-            string[] addressSplit  = fromAddress.Split(':');
+            string[] addressSplit = fromAddress.Split(':');
             string[] dataSplit = data.Split(':');
             NetworkManager.singleton.networkAddress = addressSplit[addressSplit.Length - 1];
             NetworkManager.singleton.networkPort = int.Parse(dataSplit[dataSplit.Length - 1]);
@@ -31,18 +37,38 @@ public class CustomNetworkDiscovery : NetworkDiscovery {
         }
     }
 
-    public void StartListeningBroadcast() {
+    public void StartListeningBroadcast()
+    {
         StartAsClient();
-        playernum = (int)Random.Range(1f,3.9f);
-        playNumSet = true;
+        //playernum = (int)Random.Range(1f, 3.9f);
         // accessing the game state script, check if rand num exists
+
+
+        if (player1set && player2set && player3set)
+        {
+            SetPlayerNumber(3);
+            player4set = true;
+        }
+        else if (player2set)
+        {
+            SetPlayerNumber(2);
+            player3set = true;
+        }
+        else if (player1set)
+        {
+            SetPlayerNumber(1);
+            player2set = true;
+        }
+        playNumSet = true;
 
     }
 
-    public void StartAsHost () {
+    public void StartAsHost()
+    {
         NetworkManager.singleton.StartHost();
         StartAsServer();
         SetPlayerNumber(0);
+        player1set = true;
         playNumSet = true;
     }
 
@@ -50,10 +76,12 @@ public class CustomNetworkDiscovery : NetworkDiscovery {
     {
         return playNumSet;
     }
-    public void SetPlayerNumber(float num){
+    public void SetPlayerNumber(float num)
+    {
         playernum = num;
     }
-    public float GetPLayerNumber(){
+    public float GetPLayerNumber()
+    {
         return playernum;
     }
 }
