471853e985136a1868cc012c8212e76f79091d7d
david krauskopf-greene <davidkrauskopf-greene@davids-MacBook-Pro-24.local>
2018-08-07 21:29:48 -0400

    cant score twice on hoop 4

--- Assets/Final_Project/Scripts/ShotMade.cs
+++ Assets/Final_Project/Scripts/ShotMade.cs
@@ -43,7 +43,6 @@ public class ShotMade : NetworkBehaviour
                 explode = Instantiate(explosion);
                 explode.transform.position = new Vector3(col.transform.position.x, col.transform.position.y + .275f, col.transform.position.z + .5f);
                 Destroy(col.gameObject);
-                Destroy(transform.gameObject);
                 Destroy(explode, 2);
                 col.transform.parent.gameObject.transform.GetChild(0).GetComponent<Renderer>().material.color = Color.red;
                 Destroy(col.transform.parent.gameObject, 3);
@@ -67,7 +66,6 @@ public class ShotMade : NetworkBehaviour
                 explode = Instantiate(explosion);
                 explode.transform.position = new Vector3(col.transform.position.x, col.transform.position.y + .275f, col.transform.position.z + .5f);
                 Destroy(col.gameObject);
-                Destroy(transform.gameObject);
                 Destroy(explode, 2);
                 col.transform.parent.gameObject.transform.GetChild(0).GetComponent<Renderer>().material.color = Color.red;
                 Destroy(col.transform.parent.gameObject, 3);
